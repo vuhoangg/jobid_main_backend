bf1bfd1f48cdba48ceb63ab3ad1cf4afb9643890
Magical <khanhnq.nd@gmail.com>
2020-05-03 23:52:20 +0700

    fix facebook login

--- dist/db/schemas/User.js
+++ dist/db/schemas/User.js
@@ -90,7 +90,7 @@ const userSchema = new mongoose.Schema({
         },
         salary: {
             type: Number,
-            required: true,
+            default: 0,
         },
         benefit: [
             {
