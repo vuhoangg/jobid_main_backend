9388c2b7be12ce5a46d9a1a8bb2a2b676c60de84
vietch.ndm <vietch.ndm@gmail.com>
2020-06-15 14:09:39 +0700

    edit .env-example

--- src/graphql/profile_view/resolvers/update.ts
+++ src/graphql/profile_view/resolvers/update.ts
@@ -6,7 +6,7 @@ export function updateProfileView(source, args, context, info) {
     let loggedUser = context.user;
     let input = args.input;
     input = Object.assign(input, { user_hunter: loggedUser._id });
-    return ProfileViewService.profileView(input).then(async(data) => {
+    return ProfileViewService.profileView(input).then(async (data) => {
       let notification = {
         type: "user",
         subject: "user_apply_job",
@@ -18,24 +18,23 @@ export function updateProfileView(source, args, context, info) {
         href: "",
         read: false,
       };
-      NotificationService.create(notification);
-      api("POST", `${process.env.SOCKET_URL}/socket/notify/${data.user_profile}`, {
-        params: {
-          token: "asdasfgasfasd2132",
-        },
-      }, {
-        data: await NotificationService.create(notification),
+      const n1 = await NotificationService.create(notification);
+      const params = {
+        token: process.env.SOCKET_TOKEN as string,
+      };
+      api("POST", `${process.env.SOCKET_URL}/socket/notify/${data.user_profile}`, params, {
+        data: n1,
       })
         .then((data) => console.log(data))
         .catch((e) => console.log(e));
 
-      // api("POST", `${process.env.BOT_URL}/send_notification`, {
-      //   psid: "3332232216822875",
-      //   message_type: "profile_view",
-      //   message_text: `${loggedUser.first_name} ${loggedUser.last_name} đã xem hồ sơ của bạn`,
-      // })
-      //   .then((data) => console.log(data))
-      //   .catch((e) => console.log(e));
+      api("POST", `${process.env.BOT_URL}/send_notification`, {
+        psid: "3332232216822875",
+        message_type: "profile_view",
+        message_text: `${loggedUser.first_name} ${loggedUser.last_name} đã xem hồ sơ của bạn`,
+      })
+        .then((data) => console.log(data))
+        .catch((e) => console.log(e));
 
       return data;
     });
