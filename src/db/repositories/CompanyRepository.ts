faab5b2b4aeb7de94576d9c5edca1c1de6803b1e
vietch.ndm <vietch.ndm@gmail.com>
2020-06-22 13:10:10 +0700

    populate list_user update

--- src/db/repositories/CompanyRepository.ts
+++ src/db/repositories/CompanyRepository.ts
@@ -1,11 +1,11 @@
-import {CrudContract} from "../contracts/CrudContract";
+import { CrudContract } from "../contracts/CrudContract";
 import Company from "../schemas/Company";
-import {errorLog} from "../../helpers/log";
-import {promiseNull} from "../../helpers/promise";
+import { errorLog } from "../../helpers/log";
+import { promiseNull } from "../../helpers/promise";
 
 interface ISort {
-  created?: "newest" | "oldest",
-  updated?: "newest" | "oldest",
+  created?: "newest" | "oldest";
+  updated?: "newest" | "oldest";
 }
 
 interface IFilter {
@@ -26,22 +26,24 @@ interface IGetBy {
 function getCondition(filter: IFilter) {
   let condition = {};
   if (filter.name) {
-    condition = Object.assign(condition, {$or: [{vi_name: new RegExp(filter.name, "i")}, {en_name: new RegExp(filter.name, "i")}]});
+    condition = Object.assign(condition, {
+      $or: [{ vi_name: new RegExp(filter.name, "i") }, { en_name: new RegExp(filter.name, "i") }],
+    });
   }
-  if(filter.verify_status) {
-    condition = Object.assign(condition, {verify_status: filter.verify_status});
+  if (filter.verify_status) {
+    condition = Object.assign(condition, { verify_status: filter.verify_status });
   }
-  if(filter.premium_status) {
-    condition = Object.assign(condition, {premium_status: filter.premium_status});
+  if (filter.premium_status) {
+    condition = Object.assign(condition, { premium_status: filter.premium_status });
   }
   if (filter.job_category) {
-    condition = Object.assign(condition, {job_category: filter.job_category});
+    condition = Object.assign(condition, { job_category: filter.job_category });
   }
   if (filter.job_location) {
-    condition = Object.assign(condition, {job_location: filter.job_location});
+    condition = Object.assign(condition, { job_location: filter.job_location });
   }
-  if(filter.created_by) {
-    condition = Object.assign(condition, {created_by: filter.created_by});
+  if (filter.created_by) {
+    condition = Object.assign(condition, { created_by: filter.created_by });
   }
   return condition;
 }
@@ -49,10 +51,10 @@ function getCondition(filter: IFilter) {
 function getSort(sortBy: ISort) {
   let sort = {};
   if (sortBy.created) {
-    sort = Object.assign(sort, {_id: (sortBy.created === "newest" ? "desc" : "asc")})
+    sort = Object.assign(sort, { _id: sortBy.created === "newest" ? "desc" : "asc" });
   }
   if (sortBy.updated) {
-    sort = Object.assign(sort, {updated_at: (sortBy.updated === "newest" ? "desc" : "asc")})
+    sort = Object.assign(sort, { updated_at: sortBy.updated === "newest" ? "desc" : "asc" });
   }
   return sort;
 }
@@ -70,7 +72,7 @@ class CompanyRepository implements CrudContract {
 
   create(data) {
     try {
-      console.log('data', data)
+      console.log("data", data);
       return Company.create(data);
     } catch (e) {
       errorLog(e);
@@ -99,26 +101,27 @@ class CompanyRepository implements CrudContract {
   filter(filter: IFilter, limit, page, projection) {
     try {
       let condition = getCondition(filter);
-      let sort = filter.sort_by ? getSort(filter.sort_by) : {_id: "desc"};
+      let sort = filter.sort_by ? getSort(filter.sort_by) : { _id: "desc" };
       return Company.find(condition, projection)
-      .populate('job_category')
-      .populate('job_location')
-      .populate({ 
-        path: 'list_user',
-        populate: {
-          path: 'user',
-          model: 'User'
-        }
-     })
-     .populate({ 
-      path: 'list_user',
-      populate: {
-        path: 'target_permission',
-        model: 'GroupPermission'
-      }
-   })
-      .sort(sort).skip(limit * (page - 1))
-      .limit(limit);
+        .populate("job_category")
+        .populate("job_location")
+        .populate({
+          path: "list_user",
+          populate: {
+            path: "user",
+            model: "User",
+          },
+        })
+        .populate({
+          path: "list_user",
+          populate: {
+            path: "target_permission",
+            model: "GroupPermission",
+          },
+        })
+        .sort(sort)
+        .skip(limit * (page - 1))
+        .limit(limit);
     } catch (e) {
       errorLog(e);
       return promiseNull();
@@ -128,27 +131,25 @@ class CompanyRepository implements CrudContract {
   getBy(getBy: IGetBy, projection) {
     try {
       if (getBy._id) {
-        return Company.findById(getBy._id, projection)
-          .populate('job_category')
-          .populate('job_location');
+        return Company.findById(getBy._id, projection).populate("job_category").populate("job_location");
       } else if (getBy.slug) {
-        return Company.findOne({$or: [{vi_slug: getBy.slug}, {en_slug: getBy.slug}]}, projection)
-          .populate('job_category')
-          .populate('job_location')
-          .populate({ 
-            path: 'list_user',
+        return Company.findOne({ $or: [{ vi_slug: getBy.slug }, { en_slug: getBy.slug }] }, projection)
+          .populate("job_category")
+          .populate("job_location")
+          .populate({
+            path: "list_user",
             populate: {
-              path: 'user',
-              model: 'User'
-            }
-         })
-         .populate({ 
-          path: 'list_user',
-          populate: {
-            path: 'target_permission',
-            model: 'GroupPermission'
-          }
-       })
+              path: "user",
+              model: "User",
+            },
+          })
+          .populate({
+            path: "list_user",
+            populate: {
+              path: "target_permission",
+              model: "GroupPermission",
+            },
+          });
       } else {
         return promiseNull();
       }
@@ -160,7 +161,21 @@ class CompanyRepository implements CrudContract {
 
   update(data) {
     try {
-      return Company.findByIdAndUpdate(data._id, data, {new: true});
+      return Company.findByIdAndUpdate(data._id, data, { new: true })
+        .populate({
+          path: "list_user",
+          populate: {
+            path: "user",
+            model: "User",
+          },
+        })
+        .populate({
+          path: "list_user",
+          populate: {
+            path: "target_permission",
+            model: "GroupPermission",
+          },
+        });
     } catch (e) {
       errorLog(e);
       return promiseNull();
