367cfca76799d70cd7043546aca78c5a29106f50
Khanh Nguyen <khanhnq.nd@gmail.com>
2020-07-01 14:18:33 +0700

    fix logout

--- dist/graphql/logout/resolvers/get.js
+++ dist/graphql/logout/resolvers/get.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logout = void 0;
 exports.logout = (args, context) => {
-    context.res.cookie("user", null, { expires: new Date(), domain: process.env.COOKIE_DOMAIN, httpOnly: false });
+    let expired = new Date();
+    expired.setDate(expired.getDate() - 30);
+    context.res.cookie("user", null, { expires: expired, domain: process.env.COOKIE_DOMAIN, httpOnly: false });
     if (context.isAuthenticated()) {
         context.logout();
     }
--- src/graphql/logout/resolvers/get.ts
+++ src/graphql/logout/resolvers/get.ts
@@ -1,5 +1,7 @@
 export const logout = (args, context) => {
-  context.res.cookie("user", null, {expires: new Date(), domain: process.env.COOKIE_DOMAIN, httpOnly: false});
+  let expired = new Date();
+  expired.setDate(expired.getDate() - 30);
+  context.res.cookie("user", null, {expires: expired, domain: process.env.COOKIE_DOMAIN, httpOnly: false});
   if (context.isAuthenticated()) {
     context.logout();
   }
