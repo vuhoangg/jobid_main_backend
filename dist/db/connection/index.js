26ca8262e190bc947290124eb6d07efc15666d07
tonyvuhn96 <vuvanan96@gmail.com>
2024-09-19 09:50:28 +0700

    new commit

--- dist/db/connection/index.js
+++ dist/db/connection/index.js
@@ -4,7 +4,7 @@ exports.Connection = void 0;
 const mongoose = require("mongoose");
 class Connection {
     static connect() {
-        mongoose.connect(`${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, function (error) {
+        mongoose.connect(`${process.env.DB_CONNECTION}`, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, function (error) {
             if (error) {
                 console.log("Error " + error);
             }
